9d76797c6b517080c98623c6b0887b293a52f0d5
Cong Hao <77195793+hori2012@users.noreply.github.com>
2021-10-28 22:21:06 +0700

    Update BaiTapLon-Nam2.cpp

--- BaiTapLon-Nam2.cpp
+++ BaiTapLon-Nam2.cpp
@@ -279,4 +279,11 @@ int main(){
 	list.list_sholarship();
 	return 0;
 }
-
+//menu: 
+//1 Nhap thong tin sinh vien
+//2 Xuat thong tin tat ca sinh vien
+//3 them sinh vien
+//4 xoa sinh vien
+//5 danh sach sinh vien duoc nhan hoc bong
+//6 thoat 
+//7 du tru : sua thong tin sinh vien
