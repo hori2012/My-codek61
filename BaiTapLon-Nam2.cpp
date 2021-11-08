a5902b0ec0b0571c0336f86cb9ec92b29d6197f2
Cong Hao <77195793+hori2012@users.noreply.github.com>
2021-11-08 22:07:21 +0700

    Update BaiTapLon-Nam2.cpp

--- BaiTapLon-Nam2.cpp
+++ BaiTapLon-Nam2.cpp
@@ -13,6 +13,7 @@
 #include<string>
 #include<cmath>
 #include<fstream>
+#include<cstdlib>
 using namespace std;
 
 class Profile{
@@ -24,36 +25,37 @@ class Profile{
 		friend ostream& operator << (ostream& os, Profile &pf1);
 		float getGpa();
 		string getName();
+		void set_Gpa() ;
 };
 
 istream& operator >> (istream& is, Profile &pf1){
-	cout<<"Enter of the name: ";
+	cout<<"Nhap ho ten sinh vien: ";
 	fflush(stdin);
 	getline(is, pf1.fullname);
 	cout<<endl;
-	cout<<"Enter of the mssv: ";
+	cout<<"Nhap ma so sinh vien: ";
 	fflush(stdin);
 	getline(is, pf1.mssv);
 	cout<<endl;
-	cout<<"Enter of the class: ";
+	cout<<"Nhap lop: ";
 	fflush(stdin);
 	getline(is, pf1.Class);
 	cout<<endl;
-	cout<<"Enter of the province: ";
+	cout<<"Nhap que cua sinh vien: ";
 	fflush(stdin);
 	getline(is, pf1.province);
 	cout<<endl;
-	cout<<"Enter of the gpa: ";
+	cout<<"Nhap diem trung binh: ";
 	is>>pf1.gpa;
 	cout<<endl;
 	return is;
 }
 ostream& operator << (ostream& os, Profile &pf1){
-	os<<"Fullname: "<<pf1.fullname<<endl;
-	os<<"Mssv: "<<pf1.mssv<<endl;
-	os<<"Class: "<<pf1.Class<<endl;
-	os<<"Provice: "<<pf1.province<<endl;
-	os<<"GPA: "<<pf1.gpa<<endl;
+	os<<"Ho va ten: "<<pf1.fullname<<endl;
+	os<<"MSSV: "<<pf1.mssv<<endl;
+	os<<"Lop: "<<pf1.Class<<endl;
+	os<<"Que: "<<pf1.province<<endl;
+	os<<"Diem trung binh: "<<pf1.gpa<<endl;
 	return os;
 }
 float Profile::getGpa(){
@@ -62,7 +64,13 @@ float Profile::getGpa(){
 string Profile::getName(){
 	return this->fullname;
 }
-
+void Profile::set_Gpa(){
+	float temp;
+	cout<<"Nhap diem trung binh thay the: ";
+	cin>>temp;
+	cout<<endl;
+	this->gpa=temp;
+}
 class Practise:public Profile {
 	private:
 		int lesson, allow, n_allow;
@@ -74,29 +82,32 @@ class Practise:public Profile {
 		float point();
 		void setPractise(float x);
 		float getPractise();
+		void set_Lesson();
+		void set_Allow();
+		void set_n_Allow();
 };
 
 istream& operator >> (istream& is, Practise &pt1){
-	cout<<"Enter of the sum lesson: ";
+	cout<<"Nhap tong so buoi hoc (1 ki): ";
 	is>>pt1.s_lesson;
 	cout<<endl;
-	cout<<"Enter of the lesson: ";
+	cout<<"Nhap so buoi da hoc: ";
 	is>>pt1.lesson;
 	cout<<endl;
-	cout<<"Enter of the allow: ";
+	cout<<"Nhap so buoi nghi co phep: ";
 	is>>pt1.allow;
 	cout<<endl;
-	cout<<"Enter of the not allow: ";
+	cout<<"Nhap so buoi nghi khong phep: ";
 	is>>pt1.n_allow;
 	cout<<endl;
 	return is;
 }
 ostream& operator << (ostream& os, Practise &pt1){
-	os<<"Sum lesson: "<<pt1.s_lesson<<endl;
-	os<<"Lesson: "<<pt1.lesson<<endl;
-	os<<"Allow: "<<pt1.allow<<endl;
-	os<<"Not allow: "<<pt1.n_allow<<endl;
-	os<<"Point practise: "<<pt1.getPractise()<<endl;
+	os<<"Tong so buoi: "<<pt1.s_lesson<<endl;
+	os<<"So buoi hoc: "<<pt1.lesson<<endl;
+	os<<"Nghi co phep: "<<pt1.allow<<endl;
+	os<<"Nghi khong phep: "<<pt1.n_allow<<endl;
+	os<<"Diem ren luyen: "<<pt1.getPractise()<<endl;
 	return os;
 }
 float Practise::point(){
@@ -120,6 +131,28 @@ float Practise::getPractise(){
          setPractise(point());
 	return this->practise;
 }
+void Practise::set_Lesson(){
+	int number;
+	cout<<"Nhap tong so buoi da hoc: ";
+	cin>>number;
+	cout<<endl;
+	this->lesson=number;
+}
+void Practise::set_Allow(){
+	int number;
+	cout<<"Nhap so buoi nghi co phep: ";
+	cin>>number;
+	cout<<endl;
+	this->allow=number;
+}
+void Practise::set_n_Allow(){
+	int number;
+	cout<<"Nhap so buoi nghi khong phep: ";
+	cin>>number;
+	cout<<endl;
+	this->n_allow=number;
+}
+
 class Scholarship : public Practise{
 	private:
 		string namebank;
@@ -128,15 +161,17 @@ class Scholarship : public Practise{
 		friend istream& operator >> (istream& is, Scholarship &ip1);
 		friend ostream& operator << (ostream& os, Scholarship &ip1);
 		bool check();	
+		void set_Namebank();
+		void set_Numberbank();
 };
 istream& operator >> (istream& is, Scholarship &ip1){
 	if(ip1.check()){
 		cout<<"Confirm Scholarship !!"<<endl;
-		cout<<"Enter of the name bank: ";
+		cout<<"Nhap ten ngan hang: ";
 		fflush(stdin);
 		getline(is, ip1.namebank);
 		cout<<endl;
-		cout<<"Enter of the number bank: ";
+		cout<<"Nhap so tai khoan ngan hang: ";
 		is>>ip1.numberbank;
 		cout<<endl;
 		return is;
@@ -155,6 +190,21 @@ bool Scholarship::check(){
 		return true;
 	else 
 		return false;	
+}
+void Scholarship::set_Namebank(){
+	string str;
+	cout<<"Nhap ten ngan hang: ";
+	fflush(stdin);
+	getline(cin, str);
+	cout<<endl;
+	this->namebank;
+}
+void Scholarship::set_Numberbank(){
+	int number;
+	cout<<"Nhap so tao khoan ngan hang: ";
+	cin>>number;
+	cout<<endl;
+	this->numberbank=number;
 }
 
 class Student: public Scholarship{
@@ -211,6 +261,7 @@ class Student_Management{
 		void searchName();
 		void Delete();
 		void add_student();
+		void fix_infor();
 			
 };
 
@@ -299,7 +350,7 @@ void Student_Management::traverse(){
 	Node *p=head;
 	fstream File;
 	File.open("List_student.txt", ios::out);
-	File<<"=====List Information Student======"<<endl;
+	File<<"=====DANH SACH THONG TIN SINH VIEN======"<<endl;
 	while(p!=NULL){
 		cout<<p->data;
 		File<<p->data;
@@ -311,13 +362,13 @@ void Student_Management::traverse(){
 void Student_Management::list_student(int &n){
 	Student value;
 	do{
-		cout<<"Enter the number of the students: ";
+		cout<<"Nhap so luong sinh vien: ";
 		cin>>n;
 		cout<<endl;
 	}while(n==0);
 	
 	for(int i=0;i<n;i++){
-		cout<<"Student "<<i+1<<": "<<endl;
+		cout<<"Sinh vien "<<i+1<<": "<<endl;
 		cin>>value;
 		addLast(value);
 	}
@@ -326,7 +377,7 @@ void Student_Management::list_sholarship(){
 	fstream File;
 	File.open("list_scholarship.txt", ios::out);
 	File<<"======List confirm Scholarship====== "<<endl;
-	cout<<"======List confirm Scholarship====== "<<endl;
+	cout<<"======DANH SACH HOC BONG====== "<<endl;
 	Node *p=head;
 	while(p!=NULL){
 		if(p->data.check()){
@@ -342,7 +393,7 @@ void Student_Management::list_sholarship(){
 }
 void Student_Management ::searchName(){
 	string str;
-	cout<<"Enter the name you want to search: ";
+	cout<<"Nhap ten muon tim: ";
 	fflush(stdin);
 	getline(cin, str);
 	cout<<endl;
@@ -350,13 +401,16 @@ void Student_Management ::searchName(){
 	while(p!=NULL){
 		if(p->data.getName() == str)
 			cout<<p->data;
+		else{
+			cout<<"Don't the name you want to search !!"<<endl;
+		}	
 		p=p->next;	
 	}
 	delete p;
 }
 void Student_Management::Delete(){
 	string str;
-	cout<<"Enter the name you want to delete: ";
+	cout<<"Nhap ten muon xoa: ";
 	fflush(stdin);
 	getline(cin, str);
 	cout<<endl;
@@ -371,17 +425,79 @@ void Student_Management::Delete(){
 }
 void Student_Management::add_student(){
 	int n;
-	cout<<"Enter the number af the add student: ";
+	cout<<"Nhap so luong sinh vien muon them: ";
 	cin>>n;
 	if(n==0){
 		return;
 	}
 	Student value;
 	for(int i=0; i<n;i++){
-		cout<<"Student "<<i+1<<endl;
+		cout<<"Sinh vien "<<i+1<<endl;
 		cin>>value;
 		addLast(value);
 	}	
+}
+void Student_Management::fix_infor(){
+	string str;
+	cout<<"Nhap ten sinh vien can sua thong tin: ";
+	fflush(stdin);
+	getline(cin, str);
+	cout<<endl;
+	Node *p=head;
+	while(p!=NULL){
+		if(p->data.getName() == str){
+			int choice;
+			do{
+				cout<<"-------Cac noi dung sua-------"<<endl;
+				cout<<"1. Sua diem trung binh"<<endl;
+				cout<<"2. Sua tong so buoi da hoc"<<endl;
+				cout<<"3. Sua so buoi nghi co phep"<<endl;
+				cout<<"4. Sua so buoi nghi khong phep"<<endl;
+				cout<<"5. Sua ten ngan hang"<<endl;
+				cout<<"6. Sua so tai khoan ngan hang"<<endl;
+				cout<<"Nhan phim 0 de tro lai menu chinh !!"<<endl;
+				cout<<"--->Vui long chon chuc nang: ";
+				cin>>choice;
+				cout<<endl;
+				switch(choice){
+					case 1:
+						p->data.set_Gpa();
+						break;
+					case 2:
+						p->data.set_Lesson();
+						break;
+					case 3:
+						p->data.set_Allow();
+						break;
+					case 4:
+						p->data.set_n_Allow();
+						break;
+					case 5:
+						if(p->data.check()){
+							p->data.set_Namebank();
+							break;
+						}
+						else{
+							cout<<"Doi tuong khong thuoc dien duoc sua !!"<<endl;
+						}	
+						break;
+					case 6:
+						if(p->data.check()){
+							p->data.set_Numberbank();
+							break;
+						}
+						else{
+							cout<<"Doi tuong khong thuoc dien sua !!"<<endl;
+						}	
+						break;	
+					default:
+						choice=0;
+						break;			
+				}
+			}while(choice != 0);
+		}
+		p=p->next;
+	}
 }
 int main(){
 	Student_Management list;
@@ -424,9 +540,11 @@ int main(){
 				list.traverse();
 				break;
 			case 6:
+				list.fix_infor();
 				break;
 			case 7:
 				list.searchName();	
+				break;
 			default:
 				choice=0;
 				break;				
