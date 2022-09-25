0cf8dd5a1f2258d51f4084e8daa9feac1ac39201
Cong Hao <77195793+hori2012@users.noreply.github.com>
2022-09-25 11:10:01 +0700

    Add files via upload

---
+++ QL_NhanVien/LeTan.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QL_NhanVien
+{
+    internal class LeTan : NhanVien
+    {
+        private double TiLeThuong;
+
+        public LeTan()
+        {
+
+        }
+
+        public LeTan(string MaNV, string Hoten, DateTime NgaySinh,string GioiTinh, string DiaChi, string SDT, double LuongCB, string HocVan, double TiLeThuong) : base(MaNV, Hoten, NgaySinh,GioiTinh, DiaChi, SDT, LuongCB, HocVan)
+        {
+            this.TiLeThuong1 = TiLeThuong;
+
+        }
+
+        public double TiLeThuong1 { get => TiLeThuong; set => TiLeThuong = value; }
+
+        public override string CheckType()
+        {
+            return "Le Tan";
+        }
+
+        public override void Input()
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            //nhap ma nhan vien tu dong
+            int a = 0;
+            do
+            {
+                Console.Write("Nhập họ và tên : ");
+                string name = Console.ReadLine();
+                if (eventString(name))
+                {
+                    HoTen1 = name;
+                    a = 1;
+                }
+            } while (a != 1);
+            Console.Write("\nNhập ngày sinh : ");
+            NgaySinh1 = eventTime();
+            bool flag2 = true;
+            do
+            {
+                Console.Write("\nNhập giới tính :");
+                string gender = Console.ReadLine();
+                flag2 = eventGender(gender);
+                if (flag2 == true)
+                {
+                    GioiTinh1 = gender;
+                }
+            } while (flag2 == false);
+            int b = 0;
+            do
+            {
+                Console.Write("\nNhập địa chỉ :");
+                string add = Console.ReadLine();
+                if (eventString(add))
+                {
+                    DiaChi1 = add;
+                    b = 1;
+                }
+            } while (b != 1);
+            int c = 0;
+            do
+            {
+                Console.Write("\nNhập số điện thoại :");
+                string sdt = Console.ReadLine();
+                if (KTSDT(sdt))
+                {
+                    SDT1 = sdt;
+                    c = 1;
+                }
+            } while (c != 1);
+            bool flag = true;
+            do
+            {
+                Console.Write("\nNhập lương cơ bản :");
+                double e = 0;
+                string str = "\nNhập lươngg cơ bản :";
+                flag = errorType(ref e, str);
+                if (flag == true)
+                {
+                   LuongCB1 = e;
+                }
+
+            } while (flag == false);
+            int d = 0;
+            do
+            {
+                Console.Write("\nNhập trình độ học vấn :");
+                string hocvan = Console.ReadLine();
+                if (eventString(hocvan))
+                {
+                    HocVan1 = hocvan;
+                    d = 1;
+                }
+            } while (d != 1);
+
+            //ti le thuong %
+            bool flag1 = true;
+            do
+            {
+                Console.Write("\nNhập tỉ lệ  thưởng (%) :");
+                double e = 0;
+                string str = "\nNhập tỉ lệ  thưởng (%) :";
+                flag1 = errorType(ref e, str);
+                if(flag1 == true)
+                {
+                    TiLeThuong1 = e;
+                }
+                
+            }while (flag1 == false);
+        }
+
+        public override void Output()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.WriteLine("Mã nhân viên: {0}", MaNV1);
+            Console.WriteLine("Họ và tên : {0}", HoTen1);
+            Console.WriteLine("Ngày sinh : {0}", NgaySinh1.ToString("D"));
+            Console.WriteLine("Giới tính: {0}", GioiTinh1);
+            Console.WriteLine("Địa chỉ : {0}", DiaChi1);
+            Console.WriteLine("Số điện thoại : {0}", SDT1);
+            Console.WriteLine("Lương cơ bản: {0} VND", LuongCB1);
+            Console.WriteLine("Trình độ học vấn: {0} ", HocVan1);
+            Console.WriteLine("Tỉ lệ lương thưởng: {0}%", this.TiLeThuong);
+        }
+    }
+}
---
+++ QL_NhanVien/NhanVien.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QL_NhanVien
+{
+
+    internal abstract class NhanVien
+    {
+        private string MaNV;
+        private string HoTen;
+        private DateTime NgaySinh;
+        private string GioiTinh;
+        private string DiaChi;
+        private string SDT;
+        private double LuongCB;
+        private string HocVan;
+
+        public NhanVien()
+        {
+
+        }
+
+        public NhanVien(string MaNV, string Hoten, DateTime NgaySinh, string GioiTinh, string DiaChi, string SDT, double LuongCB, string HocVan)
+        {
+            this.MaNV1 = MaNV;
+            this.HoTen1 = Hoten;
+            this.NgaySinh1 = NgaySinh;
+            this.GioiTinh = GioiTinh;
+            this.DiaChi1 = DiaChi;
+            this.SDT1 = SDT;
+            this.LuongCB1 = LuongCB;
+            this.HocVan1 = HocVan;
+        }
+
+        public string MaNV1 { get => MaNV; set => MaNV = value; }
+        public string HoTen1 { get => HoTen; set => HoTen = value; }
+        public DateTime NgaySinh1 { get => NgaySinh; set => NgaySinh = value; }
+        public string DiaChi1 { get => DiaChi; set => DiaChi = value; }
+        public string SDT1 { get => SDT; set => SDT = value; }
+        public double LuongCB1 { get => LuongCB; set => LuongCB = value; }
+        public string HocVan1 { get => HocVan; set => HocVan = value; }
+        public string GioiTinh1 { get => GioiTinh; set => GioiTinh = value; }
+
+        abstract public void Input();
+        abstract public void Output();
+        abstract public string CheckType();
+
+        static private bool KTString(char c)
+        {
+            int count = 0;
+            char[] arr = {'á', 'ả', 'ã', 'ạ', 'à', 'â', 'ấ', 'ẫ', 'ẩ', 'ậ', 'ầ', 'ă', 'ắ', 'ẵ', 'ặ', 'ẳ', 'ằ', 'ê',
+                'ế', 'ề', 'ễ', 'ệ', 'ể', 'đ', 'i','í', 'ì', 'ị', 'ĩ', 'ỉ', 'ó', 'ò', 'ỏ', 'õ' , 'ọ', 'ú', 'ù' ,'ũ',
+                'ủ', 'ụ', 'ý', 'ỳ', 'ỷ', 'ỹ', 'ỵ','ơ','ớ','ở', 'ợ', 'ỡ', 'ờ','ô','ố', 'ồ', 'ộ', 'ỗ', 'ổ','ứ', 'ừ', 'ử',
+                'ữ','ự','é', 'è', 'ẽ', 'ẻ', 'ẹ','Á', 'Ả', 'Ã', 'Ạ', 'À', 'Â', 'Ấ', 'Ẫ', 'Ẩ', 'Ậ', 'Ầ', 'Ă', 'Ắ', 'Ẵ', 'Ặ', 'Ằ', 'Ê',
+                'Ế', 'Ề', 'Ễ', 'Ệ', 'Ể', 'Đ', 'I','Í', 'Ì', 'Ị', 'Ĩ', 'Ỉ', 'Ó', 'Ò', 'Ỏ', 'Õ' , 'Ọ', 'Ú', 'Ù',
+                'Ũ', 'Ủ', 'Ụ', 'Ý', 'Ỳ', 'Ỷ', 'Ỹ','Ỵ', 'Ớ','Ở', 'Ợ', 'Ỡ', 'Ờ','Ố', 'Ồ', 'Ộ', 'Ỗ', 'Ổ','Ứ', 'Ừ',
+                'Ử', 'Ữ','Ự','É', 'È', 'Ẽ', 'Ẻ', 'Ẹ'};
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (c == arr[i])
+                {
+                    count = 1;
+                    //break;
+                }
+            }
+            if (count != 1)
+            {
+                return false;
+            }
+            return true;
+        }
+        static protected bool eventString(string str)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            int count = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (KTString(str[i]) == true)
+                {
+                    count++;
+                }
+                else if ((str[i] >= 65 && str[i] <= 90) || str[i] == 32 || ((str[i] >= 97 && str[i] <= 122)))
+                {
+                    count++;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static protected DateTime eventTime()
+        {
+            int day, month, year;
+            do
+            {
+                Console.Write("\n\tEnter day:");
+                day = Convert.ToInt32(Console.ReadLine());
+            } while (day > 31 || day < 1);
+            do
+            {
+                Console.Write("\n\tEnter month:");
+                month = Convert.ToInt32(Console.ReadLine());
+            } while (month > 12 || month <= 0);
+            do
+            {
+                Console.Write("\n\tEnter year:");
+                year = Convert.ToInt32(Console.ReadLine());
+            } while (year > (DateTime.Now).Year && year < 1960);
+            DateTime time = new DateTime(year, month, day);
+            return time;
+        }
+        static protected bool KTSDT(string sdt)
+        {
+            for (int i = 0; i < sdt.Length; i++)
+            {
+                if (sdt[i] >= 48 && sdt[i] <= 57)
+                {
+                    int count = 0;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static protected bool eventGender(string gender)
+        {
+            if ((gender != "Nam" && gender != "nam") && (gender != "Nu" && gender != "nu" && gender != "Nữ" && gender != "nữ"))
+            {
+                return false;
+            }
+            return true;
+        }
+        static protected bool errorType(ref double c, string str)
+        {
+            bool flag = true;
+            try
+            {
+                c = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Only enter number !!!!");
+                Console.Write(str);
+                flag = false;
+            }
+            finally
+            {
+                if (flag == false)
+                {
+                    errorType(ref c, str);
+                }
+            }
+            return true;
+        }
+    }
+}
